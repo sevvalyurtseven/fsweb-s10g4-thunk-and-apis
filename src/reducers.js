5bb01fe4afed5626b45f152bcfa9949323b5a804
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-23 17:26:59 +0300

    FAV_ADD case'i duzenlendi.

--- src/reducers.js
+++ src/reducers.js
@@ -25,27 +25,35 @@ function readFavsFromLocalStorage() {
 export function myReducer(state = initial, action) {
   switch (action.type) {
     case FAV_ADD:
-      const addFavState = {...state, favs: [...state.favs, action.payload]};
-      writeFavsToLocalStorage(addFavState)
-      return addFavState;
+      const oldFav = state.favs.find((item) => item.id == action.payload.id);
+      if (oldFav) {
+        return state;
+      } else {
+        const addFavState = { ...state, favs: [...state.favs, action.payload] };
+        writeFavsToLocalStorage(addFavState);
+        return addFavState;
+      }
 
     case FAV_REMOVE:
-      const removedFavState = {...state, favs: state.favs.filter(item => item.id !==action.payload)};
-      writeFavsToLocalStorage(removedFavState)
+      const removedFavState = {
+        ...state,
+        favs: state.favs.filter((item) => item.id !== action.payload),
+      };
+      writeFavsToLocalStorage(removedFavState);
       return removedFavState;
 
     case FETCH_SUCCESS:
-      return {...state, current: action.payload, error: null, loading: false};
+      return { ...state, current: action.payload, error: null, loading: false };
 
     case FETCH_LOADING:
-      return {...state, error: null, loading: true};
+      return { ...state, error: null, loading: true };
 
     case FETCH_ERROR:
-      return {...state, current: null, error: action.payload, loading: false};
+      return { ...state, current: null, error: action.payload, loading: false };
 
     case GET_FAVS_FROM_LS:
       const favFromLS = readFavsFromLocalStorage();
-      return {...state, favs: favFromLS ? favFromLS : []};
+      return { ...state, favs: favFromLS ? favFromLS : [] };
 
     default:
       return state;
