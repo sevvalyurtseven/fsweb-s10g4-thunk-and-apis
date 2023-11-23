358c27da552ce9154b967b09c22b9e033587fb60
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-23 17:34:16 +0300

    Verinin browser'da gorunmesi icin ilgili degisikligi yaptik.

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,8 @@ import React from 'react'
 function Item({ data }) {
   return (
     <div className='shadow-md bg-white text-center'>
-      <p className='text-2xl p-10'>{data.title}</p>
+      <p className='text-2xl p-10'>{data.setup}</p>
+      <p className='text-2xl p-10'>{data.punchline}</p>
     </div>
   )
 }
