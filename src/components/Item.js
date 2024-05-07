303fa4bc7644704bdb38e6f23de66555259de764
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-07 13:00:56 +0300

    add css detail

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
-import React from 'react'
-
+import React, { useState } from "react";
 
 function Item({ data }) {
+  const [showPunchline, setShowPunchline] = useState(false);
   return (
-    <div className='shadow-md bg-white text-center'>
-      <p className='text-2xl p-10'>{data.setup}</p>
-      <p className='text-2xl p-10'>{data.punchline}</p>
+    <div
+      onClick={() => setShowPunchline(!showPunchline)}
+      className="flip shadow-md bg-white text-center"
+    >
+      showPunchline : {showPunchline.toString()}
+      <div className={showPunchline ? "flip-content" : ""}>
+        <p className="flip-front text-2xl p-10">{data.setup}</p>
+        <p className="flip-back text-2xl p-10">{data.punchline}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
