2ff0c5b0a99492a304c47c5994b763b83292a381
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-07 13:10:49 +0300

    css detail

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,10 +7,11 @@ function Item({ data }) {
       onClick={() => setShowPunchline(!showPunchline)}
       className="flip shadow-md bg-white text-center"
     >
-      showPunchline : {showPunchline.toString()}
       <div className={showPunchline ? "flip-content" : ""}>
         <p className="flip-front text-2xl p-10">{data.setup}</p>
-        <p className="flip-back text-2xl p-10">{data.punchline}</p>
+        {showPunchline && (
+          <p className="flip-back text-2xl p-10">{data.punchline}</p>
+        )}
       </div>
     </div>
   );
