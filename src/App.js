c3cc8587505aa060cb39ff453a9b373908f37420
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-07 10:18:12 +0300

    edit jsx template

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import { addFav, fetchAnother, getFavsFromLocalStorage } from "./actions";
 export default function App() {
   //useSelector ile store'dan ilgili degerleri alalim:
 
-  const loading = useSelector(store => store.loading);
-  const current = useSelector(store => store.current);
-  const favs = useSelector(store => store.favs);
-  const error = useSelector(store => store.error);
+  const loading = useSelector((store) => store.loading);
+  const current = useSelector((store) => store.current);
+  const favs = useSelector((store) => store.favs);
+  const error = useSelector((store) => store.error);
+
   //dispatch tanimlayalim:
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getFavsFromLocalStorage());
     dispatch(fetchAnother());
-  }, []);  //Sayfa acildiginda API'den yeni bir öge ister. (sayfanin didMounth oldugu an)
+  }, []); //Sayfa acildiginda API'den yeni bir öge ister. (sayfanin didMounth oldugu an)
 
   //const loading = false;
   //const current = null;
@@ -31,7 +32,6 @@ export default function App() {
   function handleFetchAnother() {
     dispatch(fetchAnother());
   }
-
 
   return (
     <div className="wrapper max-w-xl mx-auto px-4">
@@ -55,17 +55,25 @@ export default function App() {
 
       <Switch>
         <Route exact path="/">
-          {loading && (<div className="bg-white p-6 text-center shadow-md">YÜKLENİYOR</div>)}
+          {loading && (
+            <div className="bg-white p-6 text-center shadow-md">YÜKLENİYOR</div>
+          )}
 
-          {error && (<div className="bg-red-600 p-6 text-center shadow-md">HATA:{error} </div>)}
+          {error && (
+            <div className="bg-red-600 p-6 text-center shadow-md">
+              HATA:{error}{" "}
+            </div>
+          )}
           {current && <Item data={current} />}
 
           <div className="flex gap-3 justify-end py-3">
-            <button onClick={handleFetchAnother}
+            <button
+              onClick={handleFetchAnother}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Başka bir tane
             </button>
+
             <button
               onClick={handleAddToFavs}
               className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
@@ -77,12 +85,15 @@ export default function App() {
 
         <Route path="/favs">
           <div className="flex flex-col gap-3">
-            {favs.length > 0
-              ? favs.map((item) => (
+            {favs.length > 0 ? (
+              favs.map((item) => (
                 <FavItem key={item.id} id={item.id} item={item} />
               ))
-              : <div className="bg-white p-6 text-center shadow-md">Henüz bir favoriniz yok</div>
-            }
+            ) : (
+              <div className="bg-white p-6 text-center shadow-md">
+                Henüz bir favoriniz yok
+              </div>
+            )}
           </div>
         </Route>
       </Switch>
